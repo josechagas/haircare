75cadf486ad37ee106c3bc884e2b7817a1f22735
José Lucas Souza das Chagas <chagasjoselucas@gmail.com>
2024-12-20 17:10:28 -0300

    Rename some folders and create AppNavigationStack

--- HairCare/Features/Home/Presentation/View/HomePage.swift
+++ HairCare/Features/Home/Presentation/View/HomePage.swift
@@ -12,54 +12,52 @@ struct HomePage<VModel: HomeViewModelProtocol>: View, WithViewModel {
     @StateObject var viewModel: VModel
     
     var body: some View {
-        HomePageNavigationPath {
-            Group {
-                switch(viewModel.loadInformationsStatus) {
-                case .none, .inProgress:
-                    ExecutionInProgressView()
-                        .transition(.opacity)
-                case .success:
-                    HomePageListView<VModel>()
+        Group {
+            switch(viewModel.loadInformationsStatus) {
+            case .none, .inProgress:
+                ExecutionInProgressView()
                     .transition(.opacity)
-                    .environmentObject(viewModel)
-                case .failed:
-                    ExecutionResultView(
-                        message: "Failed to load products",
-                        topImageSystemName: "exclamationmark.triangle",
-                        actionsContent: DefaultActionsContent(
-                            title: "Action button title",
-                            action: {
-                                Task {
-                                    await viewModel.loadInformations()
-                                }
+            case .success:
+                HomePageListView<VModel>()
+                .transition(.opacity)
+                .environmentObject(viewModel)
+            case .failed:
+                ExecutionResultView(
+                    message: "Failed to load products",
+                    topImageSystemName: "exclamationmark.triangle",
+                    actionsContent: DefaultActionsContent(
+                        title: "Action button title",
+                        action: {
+                            Task {
+                                await viewModel.loadInformations()
                             }
-                        )
+                        }
                     )
-                    .transition(.opacity)
-                }
+                )
+                .transition(.opacity)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    ProfileImageView(
-                        size: 35
-                    )
-                }
-                ToolbarItemGroup(placement: .navigationBarTrailing) {
-                    Button(action: {
-
-                    }, label: {
-                        Image(systemName: "magnifyingglass")
-                    })
-                    Button(action: {
-
-                    }, label: {
-                        Image(systemName: "ellipsis")
-                            .rotationEffect(Angle.degrees(90))
-                    })
-                }
-            }
-            .navigationTitle(R.string.homePage.new_collection())
         }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                ProfileImageView(
+                    size: 35
+                )
+            }
+            ToolbarItemGroup(placement: .navigationBarTrailing) {
+                Button(action: {
+
+                }, label: {
+                    Image(systemName: "magnifyingglass")
+                })
+                Button(action: {
+
+                }, label: {
+                    Image(systemName: "ellipsis")
+                        .rotationEffect(Angle.degrees(90))
+                })
+            }
+        }
+        .navigationTitle(R.string.homePage.new_collection())
         .animation(.easeInOut, value: viewModel.loadInformationsStatus)
         .task {
             await viewModel.loadInformations()
--- HairCare/Features/Start/Presentation/Views/StartPageBottomView.swift
+++ HairCare/Features/Start/Presentation/Views/StartPageBottomView.swift
@@ -8,28 +8,35 @@
 import SwiftUI
 
 struct StartPageBottomView: View {
-    
-    @Binding var showNextPage: Bool
-    
+
     var body: some View {
         VStack(spacing: 40) {
             BigLogoView()
-            Button(
-                action: {
-                    showNextPage = true
-                }) {
-                    Text(R.string.startPage.get_started())
-                        .padding([.leading, .trailing], 20)
-                        .padding([.top, .bottom], 5)
-                }
-                .buttonStyle(.outlined
-                                .cornerRadius(.infinity)
-                                .lineWidth(1))
-                .foregroundColor(Color.primary)
+            NavigationLink(value: AppRoute.home){
+                Text(R.string.startPage.get_started())
+                    .padding([.leading, .trailing], 20)
+                    .padding([.top, .bottom], 5)
+            }
+            .buttonStyle(.outlined
+                            .cornerRadius(.infinity)
+                            .lineWidth(1))
+            .foregroundColor(Color.primary)
+//            Button(
+//                action: {
+//                    showNextPage = true
+//                }) {
+//                    Text(R.string.startPage.get_started())
+//                        .padding([.leading, .trailing], 20)
+//                        .padding([.top, .bottom], 5)
+//                }
+//                .buttonStyle(.outlined
+//                                .cornerRadius(.infinity)
+//                                .lineWidth(1))
+//                .foregroundColor(Color.primary)
         }
     }
 }
 
 #Preview {
-    StartPageBottomView(showNextPage: .constant(false))
+    StartPageBottomView()
 }
