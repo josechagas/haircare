3f3c08e8e431da2503c9b4e721ade43186e739a6
José Lucas Souza das Chagas <chagasjoselucas@gmail.com>
2024-12-21 10:58:48 -0300

    Navigation permission

--- HairCare/Application/AppNavigationStack.swift
+++ HairCare/Application/AppNavigationStack.swift
@@ -8,17 +8,13 @@
 import SwiftUI
 
 struct AppNavigationStack: View {
-    @State var navigationPath: NavigationPath = NavigationPath()
     @State var presentStartModal: Bool = false
-
-    private let appCoordinator: AppCoordinator = AppCoordinator()
+    @State var appCoordinator: AppCoordinator = AppCoordinator()
 
     var body: some View {
-        NavigationStack(path: $navigationPath) {
+        NavigationStack(path: $appCoordinator.navigationPath) {
             appCoordinator.pageFor(route: .home)
-                .navigationDestination(for: AppRoute.self) { route in
-                    appCoordinator.pageFor(route: route)
-                }
+                .navigationDestination(for: appCoordinator)
         }
         .fullScreenCover(isPresented: $presentStartModal) {
             appCoordinator.pageFor(route: .start)
