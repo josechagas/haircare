712a9bd3388a200b2a61bc4118e5826963f8b002
José Lucas Souza das Chagas <chagasjoselucas@gmail.com>
2026-04-02 09:05:33 -0300

    Atualiza script focando em melhorar compreensão

--- scripts/check_duplicated_strings.py
+++ scripts/check_duplicated_strings.py
@@ -2,46 +2,63 @@ import json
 import os
 import sys
 
-stringsFound = {}
+def stringsOn(filePath):
+    stringsFound = {}
+    with open(filePath, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        sourceLanguage = data.get('sourceLanguage', 'en')
+        strings = data.get('strings', {})
+        
+        for key, content in strings.items():
+            localizations = content.get('localizations', {})
+            # Pega o valor da tradução na língua de origem
+            value = localizations.get(sourceLanguage, {}).get('stringUnit', {}).get('value')
+            
+            if value:
+                if value not in stringsFound:
+                    stringsFound[value] = []
+                
+                stringsFound[value].append({
+                    'path': filePath,
+                    'key': key
+                })
+    return stringsFound
+
+def mergeItems(currentItems, newItems):
+    mergedItems = currentItems
+    for key, value in newItems.items():
+        if key not in mergedItems:
+            mergedItems[key] = value
+        else:
+            mergedItems[key] += value
+    return mergedItems
+
 
 def checkForDuplicatedStrings(directory):
     #Vasculha diretorio do projeto, buscando por arquivos .xcstrings
+    stringsFound = {}
     for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith(".xcstrings"):
-                #Le arquivo registrando as strings presentes nele em 'stringsFound'
                 filePath = os.path.join(root, file)
                 try:
-                    with open(filePath, 'r', encoding='utf-8') as f:
-                        data = json.load(f)
-                        sourceLanguage = data.get('sourceLanguage', 'en')
-                        strings = data.get('strings', {})
-                        
-                        for key, content in strings.items():
-                            localizations = content.get('localizations', {})
-                            # Pega o valor da tradução na língua de origem
-                            value = localizations.get(sourceLanguage, {}).get('stringUnit', {}).get('value')
-                            
-                            if value:
-                                if value not in stringsFound:
-                                    stringsFound[value] = []
-                                
-                                stringsFound[value].append({
-                                    'path': filePath,
-                                    'key': key
-                                })
-                except Exception as e:
-                    print(f"{filePath}:1: warning: Erro ao ler arquivo: {e}")
+                    #Le arquivo registrando as strings presentes nele em 'stringsFound'
+                    print("\t👀 Lendo ", filePath)
+                    newStringsFound = stringsOn(filePath)
+                    stringsFound = mergeItems(stringsFound, newStringsFound)
+                except Exception as error:
+                    print(f"{filePath}:1: warning: Erro ao ler arquivo: {error}")
+    return stringsFound
 
 if __name__ == "__main__":
     projectDir = sys.argv[1] if len(sys.argv) > 1 else "."
-    checkForDuplicatedStrings(projectDir)
+    stringsFound = checkForDuplicatedStrings(projectDir)
     found_any_duplicate = False
+    #Itera sobre todas os valores de 'stringsFound' registrando os erros quando temos mais de um item em 'locations'
     for value, locations in stringsFound.items():
         if len(locations) > 1:
             found_any_duplicate = True
             for loc in locations:
                 print(f"{loc['path']}:1: error: Texto duplicado detectado: '{value}' (Chave: {loc['key']})")
-
     sys.exit(found_any_duplicate)
     
